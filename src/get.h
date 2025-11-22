e05bf28c2908726d8a99716649aea6e498ca9734
Shahar <107328749+niki66MC@users.noreply.github.com>
2025-11-22 19:16:52 +0200

    Fixed .env
    Removed DATA from the path

--- src/get.h
+++ src/get.h
@@ -6,11 +6,12 @@
 /**
  * @brief Retrieves content from a file using an rle_fstream.
  *
- * The base path for the data directory is read from the "DOODLE_DRIVE_DATA_PATH" environment variable.
+ * The base path for the data directory is read from the "DOODLE_DRIVE_PATH" environment variable.
  *
  * @param file_name The name of the file to retrieve.
  * @return The string content of the file.
  */
 std::string get(const std::string& file_name);
+
 
 #endif // GET_H
