270e85ae2c8d216c5dc68a1538e201ded7dc1fba
IddoBA <iddo.ba1768@gmail.com>
2025-11-24 01:11:06 +0200

    Complete implementation of search command

--- src/search.cpp
+++ src/search.cpp
@@ -1,7 +1,38 @@
 #include "search.h"
+#include "rle_fstream.h"
+#include <cstdlib>
+#include <sstream>
+#include <filesystem>
+#include <fstream>
 
-// Empty implementation file for the search function
-std::string search(const std::string& content) {
-    // Implementation goes here
-    return "";
+using namespace std;
+using namespace std::filesystem;
+
+string search(const std::string& content) {
+    const char* env = std::getenv("DOODLE_DRIVE_PATH");
+    // If environment variable does not exist, return an empty string.
+    if (!env) return "";
+
+    path directory = env;
+    if (!exists(directory) || !is_directory(directory)) return "";
+
+    std::string result = "";
+
+    for (const auto& entry : directory_iterator(directory)) {
+        if (!is_regular_file(entry.path())) continue;
+
+        std::string file_name = entry.path().filename().string();
+
+        rle_fstream fstream(directory.string(), file_name);
+        // Read file content
+        std::string file_content;
+        fstream >> file_content;
+        if (file_content.find(content) == std::string::npos) {
+            // The file does not contain "content"
+            continue;
+        }
+        // The file contains "content"
+        result += file_name + "\n";
+    }
+    return result;
 }
