864e28e67662c5069aa470481c7c56e87c43884d
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2025-11-23 22:05:54 +0000

    Implement RLE file stream.

---
+++ src/rle_fstream.cpp
@@ -0,0 +1,57 @@
+#include "rle_fstream.h"
+#include "rle.h"
+
+#include <fstream>
+#include <string>
+#include <streambuf>
+
+using namespace std;
+
+rle_fstream::rle_fstream(const string& file_path, const string& file_name)
+{
+    // save the full file path
+    path = file_path + file_name;
+}
+
+rle_fstream& rle_fstream::operator>>(string& str)
+{
+    // if the path was initialized
+    if (!path.empty())
+    {
+        // open warped fstream (input binary mode)
+        fstream.open(path, ios_base::in | ios_base::binary);
+
+        // ensure fstream has opened
+        if (fstream.is_open())
+        {
+            // input the file content into str
+            const string content((istreambuf_iterator<char>(fstream)), istreambuf_iterator<char>());
+            str = rle_decrypt(content);
+            fstream.close();
+        }
+    }
+
+    // return itself for chain calls
+    return *this;
+}
+
+rle_fstream& rle_fstream::operator<<(const string& str)
+{
+    // if the path was initialized
+    if (!path.empty())
+    {
+        // open warped fstream (input binary mode)
+        fstream.open(path, ios_base::out | ios_base::binary);
+
+        // ensure fstream has opened
+        if (fstream.is_open())
+        {
+            // output the str content into the file
+            fstream << rle_encrypt(str);
+            fstream.close();
+        }
+    }
+
+    // return itself for chain calls
+    return *this;
+}
---
+++ src/rle_fstream.h
@@ -0,0 +1,44 @@
+#ifndef RLE_FSTREAM_H
+#define RLE_FSTREAM_H
+
+#include <string>
+#include <fstream>
+
+/**
+ * @brief A custom file stream operator warper that uses RLE.
+ */
+class rle_fstream
+{
+public:
+    /**
+     * @brief Constructs an rle_fstream object.
+     *
+     * @param file_path The path to the directory containing the file.
+     * @param file_name The name of the file.
+     */
+    rle_fstream(const std::string& file_path, const std::string& file_name);
+
+    /**
+     * @brief Reads the entire contents of the file, decodes it using RLE decryption,
+     * and stores the result in the provided string.
+     *
+     * @param str A reference to the string where the decrypted content will be stored.
+     * @return A reference to itself.
+     */
+    rle_fstream& operator>>(std::string& str);
+
+    /**
+     * @brief Encrypts the provided string using RLE encryption and writes the
+     * compressed data to the file.
+     *
+     * @param str The string containing the raw data to be encrypted and written.
+     * @return A reference to itself.
+     */
+    rle_fstream& operator<<(const std::string& str);
+
+private:
+    std::fstream fstream;
+    std::string path;
+};
+
+#endif // RLE_FSTREAM_H
