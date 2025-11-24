9d64f87d08e66a42568cb4447b07503d993c07b5
niki66MC <sh.zuck13@gmail.com>
2025-11-24 19:35:09 +0200

    Added the cli to the main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+#include "cli.h"
+
+/**
+ * @brief Main entry point for the CLI application.
+ * calls cli.loop to start processing commands.
+ */
 int main(int argc, char* argv[])
 {
-    
+    CLI cli;
+    cli.cli_loop();
+    return 0;
 }
