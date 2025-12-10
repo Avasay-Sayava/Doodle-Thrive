b7d2793fc2c0ead9954999a56abdca2075cd3c19
IddoBA <iddo.ba1768@gmail.com>
2025-12-10 13:59:47 +0200

    Create tests for delete command

---
+++ src/core/storageMethods/fdelete.cpp
@@ -0,0 +1,6 @@
+#include "fdelete.h"
+
+bool fdelete(const std::string& file_name) {
+    // Empty implementation for testing
+    return false;
+}
---
+++ src/core/storageMethods/fdelete.h
@@ -0,0 +1,14 @@
+#ifndef FDELETE_H
+#define FDELETE_H
+
+#include <filesystem>
+#include <string>
+
+/**
+ * @brief Deletes the file with the specified name.
+ * @param file_name The name of the file to delete.
+ * @return true if the file was successfully deleted, false otherwise.
+ */
+bool fdelete(const std::string& file_name);
+
+#endif
