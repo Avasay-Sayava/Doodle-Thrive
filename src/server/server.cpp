715cbae79b9c383832739306471446b9db4e42e4
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2025-12-11 20:09:00 +0000

    Refactor server loop and client handling for improved readability and structure

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,4 +1,5 @@
 #include "server.h"
+#include <iostream>
     
 ddrive::Server::Server(CommandDirector cd, Executor& ex, unsigned int port) : commandDirector(cd), executor(ex), port(port)
 {
@@ -6,30 +7,41 @@ ddrive::Server::Server(CommandDirector cd, Executor& ex, unsigned int port) : co
         exit(EXIT_FAILURE);
     }
 }
+
 int ddrive::Server::initServer() {
     memset(&sin, 0, sizeof(sin));
+
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(port);
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         return -1;
     }
+
     return 0;
 }
+
 int ddrive::Server::runServer()
 {
+
     // Bind to the socket
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        close(sock);
         return -1;
     }
 
     // Listen for incoming connections from clients
     if (listen(sock, 5) < 0) {
+        close(sock);
         return -1;
     }
+    
     while (true) {
-        int client_sock = accept(sock, nullptr, nullptr);
+        struct sockaddr_in client_address;
+        socklen_t client_address_len = sizeof(client_address);
+        int client_sock = accept(sock, (struct sockaddr *)&client_address, &client_address_len);
         if (client_sock < 0) {
             continue;
         }
@@ -41,10 +53,11 @@ int ddrive::Server::runServer()
     }
     return 0;
 }
+
 void ddrive::Server::handleClient(int client_sock)
 {
+    char buffer[BUFFER_SIZE];
     while (true) {
-        char buffer[BUFFER_SIZE];
         memset(buffer, 0, BUFFER_SIZE);
 
         ssize_t bytes_received = recv(client_sock, buffer, BUFFER_SIZE - 1, 0);
@@ -55,7 +68,6 @@ void ddrive::Server::handleClient(int client_sock)
 
         std::string request_line(buffer);
         auto result = commandDirector.process(request_line);
-
         std::string response = result;
         send(client_sock, response.c_str(), response.size(), 0);
     }
