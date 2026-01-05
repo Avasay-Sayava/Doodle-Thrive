7d0d112658aeec60c066c26e923ad0cdb4e701d0
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2026-01-06 01:54:07 +0200

    create skeleton

---
+++ web/src/App/index.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./style.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/path_here" element={babel_here} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
