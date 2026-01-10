5ff5df3736f888be33f385e96d892a3b87430405
niki66MC <sh.zuck13@gmail.com>
2026-01-10 18:36:35 +0200

    a bit more changes

--- web/src/App/index.js
+++ web/src/App/index.js
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 
 import "./style.css";
 
-import HomeView from "../Drive/pages/HomeView";
-import SidePanel from "../Drive/panels/SidePanel";
+
 import SignIn from "../Sign/pages/SignIn";
 import SignUp from "../Sign/pages/SignUp";
-import StarredView from "../Drive/pages/StarredView";
+import Drive from "../Drive";
 
 function App() {
   process.env.REACT_APP_API_BASE_URL = "http://localhost:5000";
@@ -27,25 +26,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/drive/*" element={
-          <div className="app-shell">
-            <header className="app-topbar">Topbar</header>
-
-            <div className="app-body">
-              <aside className="app-sidebar"><SidePanel /></aside>
-
-              <main className="app-content">
-                <Routes>
-                    <Route index element={<HomeView />} />
-                    <Route path="starred" element={<StarredView />} />
-                    {/* <Route path="shared" element={<SharedWithMeView />} />
-                    <Route path="recent" element={<RecentView />} />
-                    <Route path="bin" element={<BinView />} />
-                    <Route path="*" element={<Navigate to="/drive" replace />} /> */}
-                  </Routes>
-              </main>
-            </div>
-
-          </div>
+          <Drive />
         } />
         <Route
           path="/signin"
