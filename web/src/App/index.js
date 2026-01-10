51f4f8c0eaae5de7d5931c03c6d18a10b29264fa
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2026-01-10 15:58:40 +0200

    Implement theme toggle functionality and update styles for dark and light themes

--- web/src/App/index.js
+++ web/src/App/index.js
@@ -1,36 +1,50 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import "./style.css";
-import SignUp from "../sign/pages/SignUp";
-import SignIn from "../sign/pages/SignIn";
+import SignUp from "../Sign/pages/SignUp";
+import SignIn from "../Sign/pages/SignIn";
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("soviet");
+
+  useEffect(() => {
+    document.querySelector(":root").setAttribute("theme", currentTheme);
+  }, [currentTheme]);
+
+  const toggleTheme = () => {
+    setCurrentTheme((prev) => (prev === "soviet" ? "pink" : "soviet"));
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/signin"
-          element={
-            <SignIn />
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <SignUp />
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <Navigate
-              to="/signin"
-              replace
-            />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <button id="toggle-theme" hidden onClick={toggleTheme} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signin"
+            element={
+              <SignIn />
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <SignUp />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Navigate
+                to="/signin"
+                replace
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
