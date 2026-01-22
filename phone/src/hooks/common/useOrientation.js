dce932ff9e422500369755a793573bdb93284c68
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2026-01-22 23:48:26 +0200

    fix package, remove comments

--- phone/src/hooks/common/useOrientation.js
+++ phone/src/hooks/common/useOrientation.js
@@ -1,6 +1,5 @@
 import { useWindowDimensions } from "react-native";
 
-/** @return {"portrait"|"landscape"} */
 export function useOrientation() {
   const { height, width } = useWindowDimensions();
   const { PORTRAIT, LANDSCAPE } = Orientation;
