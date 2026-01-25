ebbcc7937dc30e3dd86f192874828647a9beaf0e
AvasaySayava <106898024+Avasay-Sayava@users.noreply.github.com>
2026-01-25 18:00:32 +0200

    fix bad zIndex

--- phone/styles/components/common/ErrorLabel.styles.js
+++ phone/styles/components/common/ErrorLabel.styles.js
@@ -3,13 +3,11 @@ import { StyleSheet } from "react-native";
 export const styles = (theme) =>
   new StyleSheet.create({
     container: {
-      zIndex: -1,
       backgroundColor: theme.colors.primaryLight,
       padding: theme.spacing.small,
       marginBottom: theme.spacing.medium,
       borderLeftColor: theme.colors.error,
       borderLeftWidth: theme.spacing.small,
-      /* set border radius to 0 on top and small on bottom */
       borderTopLeftRadius: 0,
       borderTopRightRadius: 0,
       borderBottomLeftRadius: theme.spacing.small,
